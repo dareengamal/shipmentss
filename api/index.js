ea9899ab1b149af3c8959755819c851f311815f9
alyAzazy <105085209+alyAzazy@users.noreply.github.com>
2022-06-10 15:21:34 +0200

    add endpoint to create order

--- api/index.js
+++ api/index.js
@@ -1,126 +1,122 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-const postSchema=mongoose.Schema({
-    shippment_status:String,
-    order_id:Number,
-    address:String,
-    order_status:String
-});
-const shippment = mongoose.model('shippment',postSchema);
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require('body-parser');
+const { mongoClient } = require('./mongo');
 
 const app = express();
 
-app.use(bodyParser.json({limit : "30mb", extended : true }));
-app.use(bodyParser.urlencoded({limit : "30mb", extended : true }));
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-
-app.get('/api/shippment/:order_id', async (req, res) => {
-    const shippment = await shippment.findOneAndUpdate({ order_Id: req.params.order_id });
-    res.status(200).send({ body: shippment, message: 'Successfully retrieved shippment' });
+app.get('/api/shipments/test', (req, res) => {
+  return res.send(`Mongo URL: ${process.env.MONGO_URI}`);
 });
 
-app.post('/api/shippment', async (req, res) => {
-    try {
-        console.log('[createshippment body]', req.body)
-        const { order_id } = req.body;
-        if (!order_id) return res.status(403).send('order_id is required');
+app.get('/api/find/:search', async (req, res) => {
+  const db = await mongoClient();
+  if (!db) res.status(500).send('Systems Unavailable');
 
-        const shippment = await shippment.findOne({ order_id: order_id });
-        if (!shippment) return res.status(403).send('Document already exists, cannot create');
-
-        const shippmentStatus = 'CREATED';
-
-        const newshippmentDocument = await shippment.create({ order_id: order_id,shippment_status: shippmentStatus });
-        return res.status(200).send({ body: newshippmentDocument, message: 'Successfully created shippment' });
-    }
-    catch (e) {
-        console.log('[createshippment] e', e)
-    }
+  const { search } = req.params;
+  const results = await db.collection('shipments').find({ "order_id": { $regex: search, $options: "i" } }).toArray();
+  res.status(200).send({ body: results, message: 'Successfully retrieved search results' });
 });
-/*app.delete('/shippment', async (req, res) => {
-    try {
-        console.log('[deleteshippment body]', req.body)
-        const { order_id } = req.body;
-        if (!order_id) return res.status(403).send('order_id is required');
 
-        const shippment = await shippment.findOne({ order_id: order_id });
-        if (shippment) return res.status(403).send('Document not exists, cannot delete');
+app.get('/api/shipments/:order_id', async (req, res) => {
+  try{
+    const db = await mongoClient();
+    if (!db) res.status(500).send('Systems Unavailable');
+     // const { order_id } = createShipment.order_id;
+     console.log('[getShipment body]', req.params.order_id)
+    const { order_id} = req.params.order_id;
 
-        const shippmentStatus = 'cancelled';
 
-        const newshippmentDocument = await shippment.delete({ order_id: order_id,shippment_status: shippmentStatus });
-        return res.status(200).send({ body: newshippmentDocument, message: 'Successfully cancelled shippment' });
-    }
-    catch (e) {
-        console.log('[deleteshippment] e', e)
-    }
-});*/
+  const shipment = await db.collection('shipments').findOne({ order_id: req.params.order_id });
+  res.status(200).send({ body:req.params.order_id , message: 'Successfully retrieved shipment' });
+  }
+  catch (e) {
+    console.log('[getShipment] e', e)
+  }
 
-app.patch('/api/shippment', async (req, res) => {
-    try {
-        // const { order_id } = createShipment.order_id;
-        const { order_id } = req.body;
-        console.log('order_id', order_id);
-        if (!order_id) return res.status(403).send('order_id is required');
+});
 
-        const shippment = await shippment.findOne({ order_id: order_id });
-        if (!shippment) return res.status(403).send('could not find order_id');
-
-        // fetch shipment from db for this order_id
-        // determine what the current status is
-        // determine what the next status should be
-        // update the database with new
-        const currentshippmentStatus = shippment.shippment_status;
-        const nextshippmentStatus = {
-            CREATED: 'SHIPPED',
-            SHIPPED: 'DELIVERED'
-        }[currentshippmentStatus];
-
-        const updatedDocument = await shippment.updateOne({ order_id: order_id }, { shippment_status: nextshippmentStatus }, { returnDocument: true });
-        return res.status(200).send({ body: updatedDocument, message: 'Successfully updated order status' });
-
-    } catch (e) {
-        console.log('[updateshippment] e', e)
-    }
-
+app.post('/api/shipments', async (req, res) => {
+  try {
+    const db = await mongoClient();
+    if (!db) res.status(500).send('Systems Unavailable');
     
-});
-
-
-app.delete('/api/shippment', async (req,res) => {
-    try {
-        console.log('[cancelShippment body]', req.body)
-        const { order_id } = req.body;
-        if (!order_id) return res.status(403).send('order_id is required');
-
-        const shippment = await shippment.findOne({ order_id: order_id });
-        if (!shippment) return res.status(403).send('Shipment does not exist');
-
-        if(shippment.shippment_status === 'CREATED') {
-            await shippment.updateOne({ order_id }, { shippment_status: 'CANCELED' });
-            return res.status(200).send({ body: 'CANCELED', message: 'Your Shipment has been canceled' });
-        }
-        return res.status(200).send({ body: shippment.shipment_status, message: 'Cannot cancel this shipments' });
-    }
-    catch (e) {
-        console.log('[cancelShippment] e', e)
-    }
+    console.log('[createShipment body]', req.body)
+    const { order_id ,shipment_status} = req.body;
     
+    if (!order_id) return res.status(403).send('order_id is required');
+
+    const shipment = await db.collection('shipments').findOne({ order_id });
+    if (shipment) return res.status(403).send('Document already exists, cannot create');
+
+    const shipmentStatus = 'CREATED';
+
+    const newShipmentDocument = await db.collection('shipments').insertOne({ order_id, shipment_status: shipmentStatus });
+    return res.status(200).send({ body: {order_id,shipment_status}, message: 'Successfully created shipment' });
+  }
+  catch (e) {
+    console.log('[createShipment] e', e)
+  }
 });
 
-const CONNECTION_URL = 'mongodb+srv://dareenelsayed:Dg121212@cluster0.tr6st.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+ app.patch('/api/shipments', async (req, res) => {
+  try {
+    const db = await mongoClient();
+    if (!db) res.status(500).send('Systems Unavailable');
+     // const { order_id } = createShipment.order_id;
+     const { order_id,shipment_status } = req.body;
+   console.log('order_id', order_id);
+ if (!order_id) return res.status(403).send('order_id is required');
 
-app.listen(PORT , () => console.log (`server running on port : ${PORT}`));
+      const shipment = await db.collection('shipments').findOne({ order_id:order_id });
+      if (!shipment) return res.status(403).send('could not find order_id');
+ 
+//     // fetch shipment from db for this order_id
+//     // determine what the current status is
+//     // determine what the next status should be
+//     // update the database with new
+       //const x = {$set:{order_id:order_id}};
+     const currentShipmentStatus = shipment.shipment_status;
+    const nextShipmentStatus = {
+       "CREATED": "SHIPPED",
+       "SHIPPED": "DELIVERED"
+    }
+     [currentShipmentStatus];
+     
 
-mongoose.connect(CONNECTION_URL , {useNewUrlParser: true , useUnifiedTopology: true})
-    // .then(() => app.listen(PORT , () => console.log (`server running on port : ${PORT}`)))
-    .then(() =>  console.log (`server running on port : ${PORT}`))
-    .catch ((error) => console.log(error.message));
+ const updatedDocument = await db.collection('shipments').updateOne( {order_id:order_id} ,{$set:{ shipment_status: nextShipmentStatus} },{ returnDocument: true });
+     return res.status(200).send({ body:await db.collection('shipments').findOne({order_id:order_id}) , message: 'Successfully updated order status' });
 
+  } catch (e) {
+     console.log('[updateShipment] e', e)
+   }
+ });
 
+ app.delete('/api/shipments', async (req, res) => {
+   try {
+    const db = await mongoClient();
+    if (!db) res.status(500).send('Systems Unavailable');
+     console.log('[cancelShipment body]', req.body)
+     const { order_id } = req.body;
+     if (!order_id) return res.status(403).send('order_id is required');
 
+     const shipment = await db.collection('shipments').findOne({ order_id: order_id });
+     if (!shipment) return res.status(403).send('Shipment does not exist');
+
+     if (shipment.shipment_status === 'CREATED') {
+      await db.collection('shipments').updateOne({ order_id }, {$set:{ shipment_status: 'CANCELED' }});
+       return res.status(200).send({ body: await db.collection('shipments').findOne({order_id:order_id}), message: 'Your Shipment has been canceled' });
+     }
+     return res.status(200).send({ body: shipment.shipment_status, message: 'Cannot cancel this shipments' });   }
+      catch (e) {
+    console.log('[cancelShipment] e', e)
+   }
+ });
+
+app.listen(process.env.PORT || 5000, async () => {
+  console.log("The server is running on",5000)
+  // await connectDB();
+});
